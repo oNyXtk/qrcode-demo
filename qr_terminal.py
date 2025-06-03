b029d39838a0779175253b2a84d651ad31dc8b85
oNyXtk <assasin.outlast@gmail.com>
2025-06-03 23:18:05 +0300

    Update qr_terminal.py

--- qr_terminal.py
+++ qr_terminal.py
@@ -1,5 +1,7 @@
 import qrcode
-
+# qr_terminal.py
+# Teek: qrcode
+# Kirjeldus: prindib QR-koodi otse terminali ASCII kujul (ei loo pilti)
 # Terminal QR
 qr = qrcode.QRCode()
 qr.add_data("https://www.python.org")
